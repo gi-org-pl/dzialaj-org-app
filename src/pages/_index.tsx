41be621a7fc08446041070df8d8334172abd8c7f
Kamil <kamil.naskret.dev@gmail.com>
2026-06-04 12:48:39 +0200

    chore: remove Footer test usage from index page

--- src/pages/_index.tsx
+++ src/pages/_index.tsx
@@ -1,24 +1,7 @@
-import { Footer } from "@/components/shared/Footer/Footer";
-
 const Index = () => {
   return (
     <>
       <div>dzialaj.org</div>
-      <div className="max-w-[1200px] mx-auto px-4">
-        <Footer
-          links={[
-            {
-              href: "/",
-              label: "Regulamin",
-            },
-            {
-              href: "/",
-              label: "Polityka prywatności",
-            },
-          ]}
-          showOrganizationPanel={true}
-        />
-      </div>
     </>
   );
 };
