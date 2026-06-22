d88178d92b0a6756161fdbe97699a10461a413a2
Krzysztof Turek <krzysztof.turek007@gmail.com>
2026-06-22 21:38:48 +0200

    fix: athena showcase and svgr config to load components and resolve errors

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,31 @@ import path from "node:path";
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
+import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter()],
+  esbuild: {
+    jsx: "automatic"
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      jsx: "automatic",
+    },
+  },
+  plugins: [
+    tailwindcss(),
+    reactRouter(),
+    svgr({
+      svgrOptions: {
+        exportType: "default",
+        ref: true,
+        svgo: false,
+        titleProp: true,
+      },
+      include: "**/*.svg",
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
